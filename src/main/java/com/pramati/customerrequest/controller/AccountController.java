db204b6d8bf77a58de949ff232907c8415db4cdc
akhilesh.pandey <pandeyakhilesh300@gmail.com>
2019-01-30 19:17:19 +0530

    initial release

--- src/main/java/com/pramati/customerrequest/controller/AccountController.java
+++ src/main/java/com/pramati/customerrequest/controller/AccountController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,9 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pramati.customerrequest.exception.ResourceNotFoundException;
 import com.pramati.customerrequest.pojo.Account;
-import com.pramati.customerrequest.repository.AccountRepository;
 import com.pramati.customerrequest.service.AccountService;
 
 @RestController
@@ -26,7 +23,6 @@ public class AccountController {
 	@Autowired
 	private AccountService accountService;
 
-	
 	public void setAccountService(AccountService accountService) {
 		this.accountService = accountService;
 	}
@@ -34,28 +30,25 @@ public class AccountController {
 	// Create a new Account
 	@PostMapping("/accounts")
 	public Account createAccount(@Valid @RequestBody Account account) {
-		 this.accountService.createCustomerAccount(account);
-		 return null;
+		this.accountService.createCustomerAccount(account);
+		return null;
 	}
 
 	// edit a account
 	@PutMapping("/accounts/{id}")
-	public Account updateAccount(@PathVariable(value = "id") Long accountId, @Valid @RequestBody Account accountDetails) {
+	public Account updateAccount(@PathVariable(value = "id") Long accountId,
+			@Valid @RequestBody Account accountDetails) {
 
-
-		Account updatedAccount = this.accountService.updateCustomerAccount(accountDetails);;
+		Account updatedAccount = this.accountService.updateCustomerAccount(accountDetails);
 		return updatedAccount;
 	}
-	
-	// edit a account
-		@GetMapping("/accounts/list")
-		public List<Account> getAccounts() {
 
+	// get All accounts
+	@GetMapping("/accounts/list")
+	public List<Account> getAccounts() {
 
-			List<Account> list = this.accountService.getAllAccount();
-			return list;
-		}
-	
-	
+		List<Account> list = this.accountService.getAllAccount();
+		return list;
+	}
 
 }
--- src/main/java/com/pramati/customerrequest/dao/AccountDAOImpl.java
+++ src/main/java/com/pramati/customerrequest/dao/AccountDAOImpl.java
@@ -1,11 +1,13 @@
 package com.pramati.customerrequest.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Query;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
@@ -17,16 +19,12 @@ public class AccountDAOImpl implements AccountDAO {
 
 	private static final Logger logger = LoggerFactory.getLogger(AccountDAOImpl.class);
 
-	
-	private SessionFactory sessionFactory;
-
-	public void setSessionFactory(SessionFactory sf) {
-		this.sessionFactory = sf;
-	}
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	@Override
 	public void createCustomerAccount(Account account) {
-		Session session = this.sessionFactory.getCurrentSession();
+		Session session = entityManager.unwrap(Session.class);
 		session.persist(account);
 		logger.info("" + account);
 
@@ -35,28 +33,30 @@ public class AccountDAOImpl implements AccountDAO {
 	@Override
 	public Account updateCustomerAccount(Account account) {
 
-		Session session = this.sessionFactory.getCurrentSession();
-		 session.update(account);
-		 
-		 return account;
-		
+		Session session = entityManager.unwrap(Session.class);
+		session.update(account);
+
+		return account;
+
 	}
 
 	@Override
 	public Account findById(Long accountId) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public List<Account> getAllAccount() {
-		Session session = this.sessionFactory.getCurrentSession();
-		Query query = session.createQuery("* From ACCOUNT"); 
-		List<Account> list=query.getResultList();
-		 
+		Session session = entityManager.unwrap(Session.class);
+		Query query = session.createNativeQuery("SELECT * FROM ACCOUNT ;");
+		/*
+		 * ((org.hibernate.query.Query)
+		 * query).setResultTransformer(Transformers.aliasToBean(Account.class));
+		 * ArrayList<Account> list = (ArrayList<Account>) query.getResultList();
+		 */
+		ArrayList<Account> list = (ArrayList<Account>) query.list();
+
 		return list;
 	}
-
-	
 
 }
